0420f285d9578484a20698b6ec359d817778b14a
FukeKazki <kazkichi0906@gmail.com>
2021-09-19 13:46:16 +0900

    🧋MicroCMSを設定

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,23 @@
+require("dotenv").config({
+  path: ".env"
+})
+
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
     title: "gatsby-microcms-blog",
   },
-  plugins: ["gatsby-plugin-emotion"],
+  plugins: [
+    "gatsby-plugin-emotion",
+    {
+      resolve: "gatsby-source-microcms",
+      options: {
+        apiKey: process.env.MICROCMS_API_KEY,
+        serviceId: "fukkeblog",
+        apis: [{
+          endpoint: "techblog"
+        }]
+      }
+    }
+  ],
 };
